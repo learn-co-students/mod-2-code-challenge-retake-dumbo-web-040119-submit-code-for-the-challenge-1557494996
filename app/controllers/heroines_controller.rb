46c8ff8c42716c4b8ac052e54e8eef182e099483
Jerry Velazquez <jvr572@gmail.com>
2019-05-10 11:05:34 -0400

    "nailed it"

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,28 @@
 class HeroinesController < ApplicationController
+  before_action :get_heroine, only: [:show, :edit, :update]
   def index
     @heroines = Heroine.all
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+  private
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,33 @@
 class PowersController < ApplicationController
+  before_action :get_power, only: [:show]
   def index
     @powers = Power.all
+  end
+  def new
+    @power = Power.new
+  end
+
+  # def show
+  #   @power = Power.find(params[:id])
+  # end
+  # 
+  # def create
+  #   @power = Power.create(power_params)
+  #   if @power.valid?
+  #     redirect_to @power
+  #   else
+  #     render :new
+  #   end
+  # end
+
+
+  private
+
+  def get_power
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
   end
 end
